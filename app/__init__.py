6e2f68cd1a438313fa9aea80385c86d4e1ca8284
ilsuhov <ilsuhov@yandex-team.ru>
2017-03-20 01:04:25 +0300

    Fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import configparser
+import os
 app = Flask(__name__)
 
 config = configparser.RawConfigParser()
-config.read(['config.cfg'])
+config.read([os.path.expanduser('~/config.cfg')])
 apipath = "https://api.telegram.org/bot" + config.get("bot","bot_id") + "/"
 
 from app import views
