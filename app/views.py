5990489602765b3bdcd854dc39eab2a13cc91e09
ilsuhov <ilsuhov@yandex-team.ru>
2017-03-26 23:37:55 +0300

    Fix message

--- app/views.py
+++ app/views.py
@@ -7,19 +7,18 @@ from flask import request
 import requests
 import json
 
+help_message = u"Known commands are:\n - /ping\n - /help"
 
 def send_message(chat_id, text):
     requests.get(apipath + "sendMessage", params={"chat_id":chat_id, "text": text})
-
-
-def ping(chat_id):
-    send_message(chat_id, "pong")
 
 @app.route('/', methods=['POST'])
 def index():
     message=request.json
     if(message["message"]["text"] == "/ping"):
-        ping(message["message"]["chat"]["id"])
+        send_message(message["message"]["chat"]["id"], u"pong")
+    elif(message["message"]["text"] == "/help"):
+        send_message(message["message"]["chat"]["id"], help_message)
     else:
-        send_message(message["message"]["chat"]["id"], u"Что")
+        send_message(message["message"]["chat"]["id"], u"Unknown command\n" + help_message)
     return 'OK',200
